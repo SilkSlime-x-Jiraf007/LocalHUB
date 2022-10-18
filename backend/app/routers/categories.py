1fb29d7ca2b6bf94b055f2300ec9496e84bf9383
silkslime <silkslime@yandex.ru>
2022-10-18 22:16:47 +0300

    WIP: Refactoring

--- backend/app/routers/categories.py
+++ backend/app/routers/categories.py
@@ -1,93 +1,38 @@
-from typing import List
+from typing import List, Type
 from fastapi import APIRouter, Depends, HTTPException, status, Body
 from app.database import get_db
 from sqlalchemy.orm import Session
 from app import models
 from app.utils import response, wrapper, Response, BaseModel
 from app.auth import get_user, get_admin_user, User
-
-
-# #############################################################################################
+from crud import categories
 
 
 router = APIRouter()
 
 
-# #############################################################################################
-
-
-class Category(BaseModel):
-    id: int
+class CategoryName(BaseModel):
     category: str
-
-
-class CategoryPost(BaseModel):
-    category: str
-
-# #############################################################################################
-
-
-def category_from_db(category: models.Category):
-    return Category(**category.__dict__)
-
-
-def categories_get(db: Session) -> List[models.Category]:
-    return db.query(models.Category).order_by(models.Category.category.asc()).all()
-
-
-def category_get_by_category(db: Session, category: str) -> models.Category:
-    return db.query(models.Category).filter(models.Category.category == category).first()
-
-
-def category_get_by_id(db: Session, category_id: int) -> models.Category:
-    return db.query(models.Category).filter(models.Category.id == category_id).first()
-
-
-def category_create(db: Session, category: CategoryPost) -> bool:
-    if category_get_by_category(db, category.category) is not None:
-        return False
-    db.add(models.Category(category=category.category))
-    return True
-
-
-def category_update(db: Session, category_id: str, category: CategoryPost) -> bool:
-    db_category = category_get_by_id(db, category_id)
-    if db_category is None:
-        return False
-    db_category.category = category.category
-    return True
-
-
-def category_delete(db: Session, category_id: int) -> bool:
-    db_category = category_get_by_id(db, category_id)
-    if db_category is None:
-        return False
-    db.delete(db_category)
-    return True
-
-
-# #############################################################################################
 
 
 @router.post("/", response_model=Response)
-def add_new_category(category: CategoryPost, user: User = Depends(get_admin_user), db: Session = Depends(get_db)):
-    if category_create(db, category):
+def add_new_category(category: str = Body(), user: User = Depends(get_admin_user), db: Session = Depends(get_db)):
+    if categories.create(db, category):
         db.commit()
         return response(None, "Категория создана")
     else:
         db.rollback()
-        raise HTTPException(status.HTTP_400_BAD_REQUEST,
-                            "Такая категория уже существует")
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Такая категория уже существует")
 
 
-@router.get("/", response_model=wrapper(List[Category], "CategoriesGetResponse"))
+@router.get("/", response_model=wrapper(List[categories.Category], "CategoriesGetResponse"))
 def get_all_categories(user: User = Depends(get_user), db: Session = Depends(get_db)):
-    return response([category_from_db(category) for category in categories_get(db)])
+    return response([categories.convert(category) for category in categories.get_all(db)])
 
 
 @router.put("/{category_id}", response_model=Response)
-def rename_category(category_id: int, category: CategoryPost, user: User = Depends(get_user), db: Session = Depends(get_db)):
-    if category_update(db, category_id, category):
+def rename_category(category_id: int, category: str = Body(), user: User = Depends(get_admin_user), db: Session = Depends(get_db)):
+    if categories.update(db, category_id, category):
         db.commit()
         return response(None, "Категория изменена")
     else:
@@ -96,8 +41,8 @@ def rename_category(category_id: int, category: CategoryPost, user: User = Depen
 
 
 @router.delete("/{category_id}", response_model=Response)
-def remove_category(category_id: int, user: User = Depends(get_user), db: Session = Depends(get_db)):
-    if category_delete(db, category_id):
+def remove_category(category_id: int, user: User = Depends(get_admin_user), db: Session = Depends(get_db)):
+    if categories.delete(db, category_id):
         db.commit()
         return response(None, "Категория удалена")
     else:
